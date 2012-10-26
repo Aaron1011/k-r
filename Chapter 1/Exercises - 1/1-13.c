05e3b999dd143fd1023b840b9bd57e2ee4e5a3d1
Aaron Hill <aa1ronham@aol.com>
2012-10-25 20:35:17 -0400

    Improved Chapter 1 exercise 1-13

--- Chapter 1/Exercises - 1/1-13.c
+++ Chapter 1/Exercises - 1/1-13.c
@@ -1,43 +1,42 @@
 #include <stdio.h>
 #define IN 1    /* inside a word */
 #define OUT 0   /* outside a world */
+#define MAX_LENGTH 45 /* longest word in a major English dictionary is 45 letters */
+
 
 /* print a histogram of the lengths of words in input */
 
 main()
 {
-    int c, i, l, word, not_empty;
-    int lengths[45]; /* longest word in a major English dictionary is 45 letters */
-    word = 0;
+    int c, i, l, currentlength, not_empty;
+    int lengths[MAX_LENGTH]; /* longest word in a major English dictionary is 45 letters */
+    currentlength = 0;
 
-    for (i = 0; i < 45; ++i)
+    for (i = 0; i < MAX_LENGTH; ++i)
         lengths[i] = 0;
 
     while ((c = getchar()) != EOF)
     {
         if (c == ' ' || c == '\n' || c == '\t')
         {
-            ++lengths[word];
-            word = 0;
+            ++lengths[currentlength];
+            currentlength = 0;
         }
         else
-         ++word;
+         ++currentlength;
     }
     printf("Word lengths:\n");
     
-    for (i=1; i < 46; i++)
+    for (i=0; i < MAX_LENGTH; i++)
     {
         not_empty = (lengths[i] != 0);
         if (not_empty)
-            printf("%i: ", i);
-        for (l=0; l < lengths[i]; l++)
         {
-            if (not_empty)
+            printf("%i: ", i);
+            for (l=0; l < lengths[i]; l++)
                 printf("*");
+            printf("\n"); 
         }
-        if (not_empty)
-            printf("\n");
-        
     }
     return 0;
 }
