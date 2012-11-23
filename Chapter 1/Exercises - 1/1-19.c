8c20537e2413dbcbc0945d4c50fc4643afc28a19
Aaron Hill <aa1ronham@aol.com>
2012-11-23 16:53:20 -0500

    Improved Chapter 1 exercise 1-19

--- Chapter 1/Exercises - 1/1-19.c
+++ Chapter 1/Exercises - 1/1-19.c
@@ -16,7 +16,7 @@ int main()
     while ((len = mygetline(line, MAXLINE)) > 0)
     {
         reverse(line);
-        printf("%s", line);
+        //printf("%s", line);
     }
     return 0;
 }
@@ -28,31 +28,31 @@ int reverse(char s[])
 
     copy(copied, s);    
 
+
     for (len = 0; ; ++len)
         if (s[len] == '\n')
             break;
-    printf("%i", len);
-    for (i = 0; i <= len; ++i)
-        s[len - i] = copied[i];
-
-    
+    s[len] = '\n';
+    for (i = 0; i <= len -1; ++i)
+    {
+        if (copied[i] == '\n')
+            break;
+        s[len - 1 - i] = copied[i];
+    }
+    printf("%s", s); 
     return 0; 
 }
 int mygetline(char s[], int lim)
 {
     int c, i;
 
-    for (i = 0; c = getchar() != EOF && c != '\n'; ++i)
+    for (i = 0; (c = getchar()) != EOF && (c != '\n'); ++i)
     {
         if (i < lim - 1)
             s[i] = c;
     }
-    if (c == '\n')
-    {
-        s[i] = c;
-        ++i;
-    }
-    s[i] = '\0';
+    s[i] = c;
+    s[i + 1] = '\0';
     return i;
 }
 
