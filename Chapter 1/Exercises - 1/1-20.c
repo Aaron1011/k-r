c25f49b4053053716603d3e0f75dd60ac3354efc
Aaron Hill <aa1ronham@aol.com>
2012-11-24 17:25:48 -0500

    Improved Chapter 1 exercie 1-20

--- Chapter 1/Exercises - 1/1-20.c
+++ Chapter 1/Exercises - 1/1-20.c
@@ -6,19 +6,26 @@ main()
 {
     int c, i, l;
     char str[MAXLINE];
+    for (i = 0; i <= MAXLINE; ++i)
+        str[i] = 0;
 
     i = 0;
 
     while ((c = getchar()) != EOF)
     {
         if  (c == '\t')
+        {
             for (l = 0; l < TABSTOP; ++l)
-            {
                 str[i+l] = ' ';
-            }
+            i += l;
+        }
         else
-            str[i] = c;
+            str[i] = c;//printf("c = %cZ, str[i]= %cZ %i\n", c, str[i], i);
         ++i;
     }
-    printf("%s\n", str);
+    str[i] = '\0';
+    for (l = 0; l <= i; l++)
+        printf("%c", str[l]);
+    //printf("Final value: %s\n", str);
+
 }
