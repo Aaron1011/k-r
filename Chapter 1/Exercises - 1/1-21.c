544d4c7c32c3402d1026ee57f284851c6de65bd4
Aaron Hill <aa1ronham@aol.com>
2012-11-24 10:06:59 -0500

    Improved Chapter 1 exercise 1-20

--- Chapter 1/Exercises - 1/1-21.c
+++ Chapter 1/Exercises - 1/1-21.c
@@ -6,6 +6,8 @@ main()
 {
     int c, i, spacecnt;
     char str[MAXLINE];
+    for (i = 0; i <= MAXLINE; ++i)
+        str[i] = 0;
     spacecnt = 0;
     i = 0;
     while ((c = getchar()) != EOF)
@@ -18,11 +20,15 @@ main()
             ++i;
         }
         if (c == ' ')
+        {
+            str[i] = c;
             ++spacecnt;
+        }
         else
             str[i] = c;
         ++i;
 
     }
+    str[i - 1] = '\0';
     printf("%s\n", str);
 }
