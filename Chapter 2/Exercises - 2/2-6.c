018fcd9d472cb2d66c6878c8c152624abb1b014c
Aaron Hill <aa1ronham@aol.com>
2012-12-22 15:50:31 -0500

    Improved Chapter 2 exercise 2-4

--- Chapter 2/Exercises - 2/2-6.c
+++ Chapter 2/Exercises - 2/2-6.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 
 unsigned setbits(unsigned x, int p, int n, unsigned y);
+unsigned create_mask(int p, int n);
 
 int main()
 {
-    printf("Setbits with x = 1101101, p = 5, n = 3, y = 1000\n");
-    printf("%i\n", setbits(1101101, 5, 3, 1000));
+    printf("Setbits with x = 1101101, p = 5, n = 3, y = 001\n");
+    printf("%i\n", setbits(0x6D, 5, 3, 1));
     return 0;
 }
 
 unsigned setbits(unsigned x, int p, int n, unsigned y)
 {
-    unsigned tempx;
+    unsigned mask, masked_x, masked_y;
 
-    tempx = x >> (p+1-n);
-	tempx |= ~(~0 << n);
-    y &= ~(~0 << n);
-    x = tempx << (p+1-n);
-    return x & y;
+    y <<= p+1-n;
+    mask = create_mask(p, n);
+    masked_x = x & ~mask;
+    masked_y = y & mask;
+    return masked_x | masked_y;
+}
+
+unsigned create_mask(int p, int n)
+{
+    return ~(~0  << n) << p+1-n;
 }
