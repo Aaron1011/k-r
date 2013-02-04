bb197126d747692cd52f396164eb58b8f25329e0
Aaron Hill <aa1ronham@aol.com>
2013-02-04 17:08:35 -0500

    Chapter 3 exercise 3-5

---
+++ Chapter-3/3-5.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#define MAXCHAR 1000
+
+
+void itob(int n, char s[], int b);
+void reverse(char s[]);
+
+int main() {
+    char s[MAXCHAR];
+
+    itob(255, s, 16);
+    printf("%s\n", s);
+    return 0;
+}
+
+
+
+void itob(int n, char s[], int b) {
+
+    int cur_num, i;
+    char CHARS[MAXCHAR] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    i = 0;
+    cur_num = n % b;
+
+    while (n > 0) {
+        s[i] = CHARS[cur_num];
+        n /= b;
+        cur_num = n % b;
+        i++;
+    }
+    s[i] = '\0';
+    reverse(s);
+}
+
+void reverse(char s[]) {
+    int c, i, j;
+
+    for (i = 0, j = strlen(s)-1; i < j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
