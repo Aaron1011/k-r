af26b5f6927be2936f5cb1ce6015f89f4300399b
Aaron Hill <aa1ronham@aol.com>
2013-02-10 10:39:01 -0500

    Improved Chapter 3 exercise 3-6

--- Chapter-3/3-6.c
+++ Chapter-3/3-6.c
@@ -3,7 +3,7 @@
 #define MAXLINE 1000
 
 
-void itoa(int n, char s[]);
+void itoa(int n, char s[], int minwidth);
 void reverse(char s[]);
 
 int main() {
@@ -12,12 +12,12 @@ int main() {
 
     for (i = 0; i <= MAXLINE; i++)
         s[i] = 0;
-    itoa(-123, s);
+    itoa(123, s, 5);
     printf("%s\n", s);
 }
 
-void itoa(int n, char s[]) {
-    int i, sign;
+void itoa(int n, char s[], int minwidth) {
+    int i, sign, original_length;
 
     if ((sign = n) < 0)
         n = -n;
@@ -29,9 +29,24 @@ void itoa(int n, char s[]) {
         s[i++] = '-';
     s[i] = '\0';
     reverse(s);
+
+    if (strlen(s) < minwidth) {
+        original_length = strlen(s);
+        for (i = original_length-1; i >= 0; i--)
+            s[i+(minwidth - original_length)] = s[i];
+    }
+    s[minwidth] = '\0';
+    for (i = 0; i < minwidth - original_length; i++)
+        s[i] = ' ';
+
 }
 
 void reverse(char s[]) {
-    int i, j = 0;
+    int c, i, j;
 
-
+    for (i = 0, j = strlen(s)-1; i < j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
